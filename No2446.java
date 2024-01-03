85991a811982f35300e54291074548861bdc622b
JuwoenLee <82970714+JuwoenLee@users.noreply.github.com>
2024-01-03 19:13:40 +0900

    Update No2446.java

--- No2446.java
+++ No2446.java
@@ -2,7 +2,7 @@ package BeakjoonSolving;
 
 import java.util.Scanner;
 
-public class Main {
+public class No2446 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
