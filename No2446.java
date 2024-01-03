19caaf2d4ba0a81423727db6044a61ecbbb9acb3
LeeJuWon <woen1016@gmail.com>
2024-01-03 19:24:36 +0900

    Revert "Update No2446.java"
    This reverts commit 85991a811982f35300e54291074548861bdc622b.

--- No2446.java
+++ No2446.java
@@ -2,7 +2,7 @@ package BeakjoonSolving;
 
 import java.util.Scanner;
 
-public class No2446 {
+public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
